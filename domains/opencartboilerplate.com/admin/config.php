87f3fb256543f58923a505a3ef10f4b763b34699
Longlife Games <po4tajurajkeee@gmail.com>
2021-11-18 22:23:32 +0200

    Opencart Setup

---
+++ domains/opencartboilerplate.com/admin/config.php
@@ -0,0 +1,36 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://opencartboilerplate.com/admin/');
+define('HTTP_CATALOG', 'http://opencartboilerplate.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://opencartboilerplate.com/admin/');
+define('HTTPS_CATALOG', 'http://opencartboilerplate.com/');
+
+// DIR
+define('DIR_APPLICATION', 'F:/OpenServer/domains/opencartboilerplate.com/admin/');
+define('DIR_SYSTEM', 'F:/OpenServer/domains/opencartboilerplate.com/system/');
+define('DIR_IMAGE', 'F:/OpenServer/domains/opencartboilerplate.com/image/');
+define('DIR_STORAGE', DIR_SYSTEM . 'storage/');
+define('DIR_CATALOG', 'F:/OpenServer/domains/opencartboilerplate.com/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'opencartdb');
+define('DB_PASSWORD', 'yrebevaty2123');
+define('DB_DATABASE', 'opencartdb');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
---
+++ domains/opencartboilerplate.com/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://opencartboilerplate.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://opencartboilerplate.com/');
+
+// DIR
+define('DIR_APPLICATION', 'F:/OpenServer/domains/opencartboilerplate.com/catalog/');
+define('DIR_SYSTEM', 'F:/OpenServer/domains/opencartboilerplate.com/system/');
+define('DIR_IMAGE', 'F:/OpenServer/domains/opencartboilerplate.com/image/');
+define('DIR_STORAGE', DIR_SYSTEM . 'storage/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/theme/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'opencartdb');
+define('DB_PASSWORD', 'yrebevaty2123');
+define('DB_DATABASE', 'opencartdb');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
